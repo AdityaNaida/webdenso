b5e18e6086ff12618ee7a961054da580a5550773
AdityaNaida <adityanaida@gmail.com>
2024-05-23 16:01:29 +0530

    [ADD] Home Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
 const servicesContainer = document.querySelector(".services-container");
-
 function viewServices() {
   servicesContainer.classList.toggle("flex");
 }
-
 document.addEventListener("click", (e) => {
   if (
     !servicesContainer.contains(e.target) &&
@@ -26,7 +24,6 @@ function viewMenu(imgSrc) {
 
 let slideIndex = 1;
 showSlides(slideIndex);
-
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
@@ -46,12 +43,24 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "flex";
   dots[slideIndex - 1].className += " active";
 }
-
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
-
 setInterval(() => {
   slideIndex++;
   showSlides(slideIndex);
 }, 3000);
+
+const faqs = document.querySelectorAll(".faq-question");
+faqs.forEach((e) => {
+  e.addEventListener("click", () => {
+    const nextDiv = e.nextElementSibling;
+    nextDiv.classList.toggle("block-faq");
+    const img = e.querySelector("img");
+    if (nextDiv.classList.contains("block-faq")) {
+      img.src = "/assets/icons/minus.svg";
+    } else {
+      img.src = "/assets/icons/plus.svg";
+    }
+  });
+});
